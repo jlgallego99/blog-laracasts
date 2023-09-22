3a9a72f1596ab1d5eabda1ff99ec9cdbc9a6bd36
jlgallego99 <jlgallego99@gmail.com>
2023-09-22 12:58:37 +0200

    Post model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,48 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<article>
-    <h1><a href="/post/my-first-post">My First Post</a></h1>
-
-    <p>
-        Lorem fistrum torpedo sexuarl amatomaa la caidita no te digo trigo por no llamarte Rodrigor quietooor mamaar.
-        Ese que llega me cago en tus muelas quietooor diodeno. Ese que llega a peich papaar papaar al ataquerl caballo
-        blanco caballo negroorl sexuarl amatomaa por la gloria de mi madre la caidita apetecan. Me cago en tus muelas te
-        voy a borrar el cerito por la gloria de mi madre te va a hasé pupitaa a wan diodeno. Pupita llevame al sircoo a
-        gramenawer a wan por la gloria de mi madre caballo blanco caballo negroorl tiene musho peligro te va a hasé
-        pupitaa. Se calle ustée te va a hasé pupitaa por la gloria de mi madre se calle ustée ese que llega a wan.
-        Caballo blanco caballo negroorl diodenoo diodeno se calle ustée ese hombree a gramenawer te va a hasé pupitaa al
-        ataquerl fistro.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-second-post">My Second Post</a></h1>
-
-    <p>
-        Lorem fistrum torpedo sexuarl amatomaa la caidita no te digo trigo por no llamarte Rodrigor quietooor mamaar.
-        Ese que llega me cago en tus muelas quietooor diodeno. Ese que llega a peich papaar papaar al ataquerl caballo
-        blanco caballo negroorl sexuarl amatomaa por la gloria de mi madre la caidita apetecan. Me cago en tus muelas te
-        voy a borrar el cerito por la gloria de mi madre te va a hasé pupitaa a wan diodeno. Pupita llevame al sircoo a
-        gramenawer a wan por la gloria de mi madre caballo blanco caballo negroorl tiene musho peligro te va a hasé
-        pupitaa. Se calle ustée te va a hasé pupitaa por la gloria de mi madre se calle ustée ese que llega a wan.
-        Caballo blanco caballo negroorl diodenoo diodeno se calle ustée ese hombree a gramenawer te va a hasé pupitaa al
-        ataquerl fistro.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/post/my-third-post">My Third Post</a></h1>
-
-    <p>
-        Lorem fistrum torpedo sexuarl amatomaa la caidita no te digo trigo por no llamarte Rodrigor quietooor mamaar.
-        Ese que llega me cago en tus muelas quietooor diodeno. Ese que llega a peich papaar papaar al ataquerl caballo
-        blanco caballo negroorl sexuarl amatomaa por la gloria de mi madre la caidita apetecan. Me cago en tus muelas te
-        voy a borrar el cerito por la gloria de mi madre te va a hasé pupitaa a wan diodeno. Pupita llevame al sircoo a
-        gramenawer a wan por la gloria de mi madre caballo blanco caballo negroorl tiene musho peligro te va a hasé
-        pupitaa. Se calle ustée te va a hasé pupitaa por la gloria de mi madre se calle ustée ese que llega a wan.
-        Caballo blanco caballo negroorl diodenoo diodeno se calle ustée ese hombree a gramenawer te va a hasé pupitaa al
-        ataquerl fistro.
-    </p>
-</article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
