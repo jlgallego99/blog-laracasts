3fbcfb8d504615a31d9e8594d03ce9ba06d13d42
jlgallego99 <jlgallego99@gmail.com>
2023-09-23 13:27:39 +0200

    Components and some fixes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,6 +26,16 @@ class Post {
         return static::all()->firstWhere('slug', $slug);
     }
 
+    public static function findOrFail($slug) {
+        $post = static::find($slug);
+        if (!$post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+    }
+
+
     public static function all() {
         return cache()->rememberForever('posts.all', function() {
             return collect(File::files(resource_path("posts")))
