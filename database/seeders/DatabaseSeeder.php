17f63799da38798428856329318fe5a5e467c14d
jlgallego99 <jlgallego99@gmail.com>
2023-09-23 20:03:09 +0200

    Seed random data for posts, authors and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,43 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+           'name' => 'Jose Luis Gallego'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem fistrum va usté muy cargadoo caballo blanco caballo negroorl papaar papaar pupita va usté muy cargadoo benemeritaar papaar papaar la caidita qué dise usteer.',
-            'body' => '<p>Lorem fistrum va usté muy cargadoo caballo blanco caballo negroorl papaar papaar pupita va usté muy cargadoo benemeritaar papaar papaar la caidita qué dise usteer. Se calle ustée mamaar quietooor está la cosa muy malar la caidita te voy a borrar el cerito ese pedazo de a wan hasta luego Lucas no puedor. Caballo blanco caballo negroorl amatomaa al ataquerl ese pedazo de no te digo trigo por no llamarte Rodrigor a wan mamaar fistro fistro benemeritaar hasta luego Lucas. Se calle ustée pecador papaar papaar caballo blanco caballo negroorl diodenoo te va a hasé pupitaa a wan no te digo trigo por no llamarte Rodrigor caballo blanco caballo negroorl. Llevame al sircoo por la gloria de mi madre de la pradera ese que llega por la gloria de mi madre a gramenawer. Me cago en tus muelas fistro tiene musho peligro te va a hasé pupitaa torpedo de la pradera ese que llega. </p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem fistrum va usté muy cargadoo caballo blanco caballo negroorl papaar papaar pupita va usté muy cargadoo benemeritaar papaar papaar la caidita qué dise usteer.',
-            'body' => '<p>Lorem fistrum va usté muy cargadoo caballo blanco caballo negroorl papaar papaar pupita va usté muy cargadoo benemeritaar papaar papaar la caidita qué dise usteer. Se calle ustée mamaar quietooor está la cosa muy malar la caidita te voy a borrar el cerito ese pedazo de a wan hasta luego Lucas no puedor. Caballo blanco caballo negroorl amatomaa al ataquerl ese pedazo de no te digo trigo por no llamarte Rodrigor a wan mamaar fistro fistro benemeritaar hasta luego Lucas. Se calle ustée pecador papaar papaar caballo blanco caballo negroorl diodenoo te va a hasé pupitaa a wan no te digo trigo por no llamarte Rodrigor caballo blanco caballo negroorl. Llevame al sircoo por la gloria de mi madre de la pradera ese que llega por la gloria de mi madre a gramenawer. Me cago en tus muelas fistro tiene musho peligro te va a hasé pupitaa torpedo de la pradera ese que llega. </p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
