882c0bdcadce9823e56b6c04db556c24dc1d81df
jlgallego99 <jlgallego99@gmail.com>
2023-10-09 20:27:43 +0200

    Register validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,11 +12,11 @@ class RegisterController extends Controller {
     public function store() {
         $attributes = request()->validate([
            'name' => 'required|max:255',
-           'username' => 'required|max:255|min:3',
-           'email' => 'required|email|max:255',
+           'username' => 'required|max:255|min:3|unique:users,username',
+           'email' => 'required|email|max:255|unique:users,email',
            'password' => 'required|max:255|min:7'
         ]);
-        
+
         User::create($attributes);
 
         return redirect('/');
