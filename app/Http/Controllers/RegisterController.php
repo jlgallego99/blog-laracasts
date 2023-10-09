657c924f023ced4b29747233bd053bdb4ff9917b
jlgallego99 <jlgallego99@gmail.com>
2023-10-09 20:05:17 +0200

    Registering user

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller {
+    public function create() {
+        return view('register.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+           'name' => 'required|max:255',
+           'username' => 'required|max:255|min:3',
+           'email' => 'required|email|max:255',
+           'password' => 'required|max:255|min:7'
+        ]);
+        
+        User::create($attributes);
+
+        return redirect('/');
+    }
+}
