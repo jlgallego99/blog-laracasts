e36d0a6298fd569da0281e4be3cafa3ab969acf8
jlgallego99 <jlgallego99@gmail.com>
2023-10-13 17:34:29 +0200

    Register newsletter service on AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,25 @@
 
 namespace App\Providers;
 
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register(): void {
+        app()->bind(Newsletter::class, function() {
+            $client = (new ApiClient())->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us21'
+            ]);
+
+            return new Newsletter($client);
+        });
     }
 
     /**
