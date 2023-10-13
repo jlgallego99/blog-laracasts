ea911e30171ea857ee6d6c9bd45b91eb7d040db0
jlgallego99 <jlgallego99@gmail.com>
2023-10-13 17:59:10 +0200

    Admins middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller {
     public function index() {
@@ -18,6 +19,12 @@ class PostController extends Controller {
     public function show(Post $post) {
         return view('posts.show', [
             'post' => $post
+        ]);
+    }
+
+    public function create() {
+        return view('posts.create', [
+
         ]);
     }
 }
