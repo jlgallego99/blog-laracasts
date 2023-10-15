aaad1ef262bf062d580574dac0456c2515fe031f
jlgallego99 <jlgallego99@gmail.com>
2023-10-15 19:26:31 +0200

    Laravel gate authorization
    The end

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,15 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 Route::post('newsletter', NewsletterController::class);
 
-
 // ADMIN
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin')->name('create_post');
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin')->name('all_posts');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
+// Middleware can be replaced with the middleware 'can:admin' because a Gate authorization has been defined
+Route::middleware('admin')->group(function () {
+    // This all can be replaced with:
+    // Route::resource('admin/posts', AdminPostController::class)->except('show');
+    Route::get('admin/posts/create', [AdminPostController::class, 'create'])->name('create_post');
+    Route::post('admin/posts', [AdminPostController::class, 'store']);
+    Route::get('admin/posts', [AdminPostController::class, 'index'])->name('all_posts');
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+});
